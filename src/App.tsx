3188e17d9bd729bb405ed818b4b66cc6c306644d
brian.musili <brian.musili@systechafrica.com>
2025-01-01 06:02:39 +0300

    Zod validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import MyZod from "./features/MyZod";
+
 function App() {
   return (
     <main className="min-h-screen w-5/6 mx-auto ">
@@ -5,6 +7,10 @@ function App() {
         <p className="text-3xl w-1/2 mx-auto ">
           Welcome to my playground, just playing around with react and UI
         </p>
+      </div>
+      {/* components */}
+      <div>
+        <MyZod/>
       </div>
     </main>
   );
