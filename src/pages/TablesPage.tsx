167b24ce934fcf711b9fb2337d2983861c7ef8d4
brian.musili <brian.musili@systechafrica.com>
2025-01-02 19:30:38 +0300

    shadcn/tanstack query tables

--- src/pages/TablesPage.tsx
+++ src/pages/TablesPage.tsx
@@ -1,9 +1,14 @@
+import { columns } from "@/features/tables/Columns";
+import { DataTable } from "@/features/tables/DataTable";
+import { payments } from "@/features/tables/PaymentsData";
 
 
 function TablesPage() {
   return (
-    <div>TablesPage</div>
-  )
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={payments} />
+    </div>
+  );
 }
 
 export default TablesPage
